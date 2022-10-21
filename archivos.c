d7d52a986e0b06c59083c09f60769edcf59929a9
NicoleOcampo <104442352+nikiOcampo@users.noreply.github.com>
2022-10-21 20:23:44 -0300

    texto a binario

--- archivos.c
+++ archivos.c
@@ -2,92 +2,70 @@
 #include <string.h>
 #include "archivos.h"
 
-
-/******************************************************************************
-//  Nombre:			binario_a_texto.
-//  Descripción:	Arma un archivo de texto a partir de un archivo binario.
-//  Parametros: 	ptxt: puntero a archivo de texto.
-//                  pbin: puntero a archivo binario.
-//  Retorno         No posee.
-******************************************************************************/
-
-                       /*{32691574,"Pepe Galleta1",{18,10,1986},'M'},
-                         {354,"Pepita Galletita2",{11,12,90},'F'}*/
-
-                       /*32691574,"Pepe Galleta1",{18,10,1986},'M'*/
-
-/*  long dni;
-    char apyn[40];
-    t_fecha fecha_nac;
-    char sexo;*/
-
-void binario_a_texto(FILE *pbin,FILE *ptxt)
+int crea_textoF()
 {
-    t_dato dato;
-
-    printf("\nESTA CONVIRTIENDO ARCHIVO BINARIO A TEXTO LONGITUD VARIABLE/FIJA\n");
-    while(fread(&dato,sizeof(t_dato),1,pbin))
-    {
-        /// Conversión de Binario a Texto de Longitud Variable
-        fprintf(ptxt,"%ld|%s|%d/%d/%d|%c\n",dato.dni,dato.apyn,dato.fecha_nac.dia,dato.fecha_nac.mes,dato.fecha_nac.anio,dato.sexo);
-
-        /// Conversión de Binario a Texto de Longitud FIJA
-
-        ///fprintf(ptxt, "%-8ld%-39s%2d%2d%4d%c\n",dato.dni,dato.apyn,dato.fecha_nac.dia,dato.fecha_nac.mes,dato.fecha_nac.anio,dato.sexo);
-
-
-    /*long dni;
-    char apyn[40];
-    t_fecha fecha_nac;
-    char sexo;
-    */
-    }
-    fclose(pbin);
-    fclose(ptxt);
-}
-
-/*****************************************************************************/
-int crea_binario()
-{
-    FILE *pbin;
+    FILE *ptxt;
     t_dato vdatos[] = {
                         {32691574,"Pepe Galleta1",{18,10,1986},'M'},
                         {354,"Pepita Galletita2",{11,12,90},'F'}
                       };
 
-    pbin = fopen("archivo_binario.bin","wb");
+    ptxt = fopen("archivo_textoFijo.txt","wb");
 
-    if(!pbin)
+    if(!ptxt)
     {
         printf("Error de creacion de archivo binario\n");
         return 1;
     }
+    for(int i=0;i<2;i++)
+        fprintf(ptxt, "%-8ld%-39s%2d%2d%4d%c\n", vdatos[i].dni, vdatos[i].apyn, vdatos[i].fecha_nac.dia,vdatos[i].fecha_nac.mes,vdatos[i].fecha_nac.anio, vdatos[i].sexo);
 
-    fwrite(&vdatos,sizeof(vdatos),1,pbin);
+    /*  long dni;
+    char apyn[40];
+    t_fecha fecha_nac;
+    char sexo;*/
 
-    fclose(pbin);
+    fclose(ptxt);
 
     return 0;
 }
 
-/*****************************************************************************/
-int muestra_binario()
+
+void textoFijoABinario(FILE *ptxt,FILE *pbin)
 {
     t_dato dato;
-    FILE *pbin = fopen("archivo_binario.bin","rb");
 
-    if(!pbin)
+    printf("\nESTA CONVIRTIENDO ARCHIVO FIJO A BINARIO\n");
+    while(fscanf(ptxt,"%8ld%39[^\n]%2d%2d%4d%c\n", &dato.dni,&dato.apyn,&dato.fecha_nac.dia,&dato.fecha_nac.mes,&dato.fecha_nac.anio,&dato.sexo)==6)
+    {
+        /// Conversión de Texto Fijo a Binario
+        fwrite(&dato,sizeof(t_dato),1,pbin);
+    }
+    fclose(pbin);
+    fclose(ptxt);
+}
+
+
+
+
+/*****************************************************************************/
+int muestra_textoF()
+{
+    t_dato dato;
+    FILE *ptxt = fopen("archivo_textoFijo.txt","rb");
+
+    if(!ptxt)
     {
         printf("Error de apertura\n");
         return 1;
     }
 
-    printf("\n MUESTRA ARCHIVO BINARIO  \n \n");
-    while(fread(&dato,sizeof(t_dato),1,pbin))
+    printf("\n MUESTRA ARCHIVO TEXTO LONGITUD FIJA  \n \n");
+    while(fscanf(ptxt,"%8ld%39[^\n]%2d%2d%4d%c\n", &dato.dni,&dato.apyn,&dato.fecha_nac.dia,&dato.fecha_nac.mes,&dato.fecha_nac.anio,&dato.sexo)==6)
+        printf("%8ld%39s%2d%2d%4d%c\n", dato.dni,dato.apyn,dato.fecha_nac.dia,dato.fecha_nac.mes,dato.fecha_nac.anio,dato.sexo);
 
-        printf("%ld|%s|%d/%d/%d|%c\n",dato.dni,dato.apyn,dato.fecha_nac.dia,dato.fecha_nac.mes,dato.fecha_nac.anio,dato.sexo);
 
-    fclose(pbin);
+    fclose(ptxt);
 
     return 0;
 }
