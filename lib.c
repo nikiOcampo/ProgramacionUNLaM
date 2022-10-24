091b38d0ec6d8d3d1b3cca27ddeb2d5668367455
NicoleOcampo <104442352+nikiOcampo@users.noreply.github.com>
2022-10-23 21:29:46 -0300

    MyStrlen, MyStrcpy ,myStrchr, myStrcat

--- lib.c
+++ lib.c
@@ -3,12 +3,12 @@
 #include "lib.h"
 
 
-///Crear una funciÛn que concatene 2 cadenas de caracteres similar a strcat, pero con la particularidad de
+///Crear una funci√≥n que concatene 2 cadenas de caracteres similar a strcat, pero con la particularidad de
 ///que, debiendo seguir concatenando en la cadena1, debe poner la cadena 2 por delante.
 ///Ej:
-///cad1 = ìmundo!î cad2 = ìHola î
+///cad1 = ‚Äúmundo!‚Äù cad2 = ‚ÄúHola ‚Äù
 ///strrcat_mio(cad1, cad2);
-///Resultado: cad1 => ìHola mundo!î
+///Resultado: cad1 => ‚ÄúHola mundo!‚Äù
 
 
 char* strcat_mio(char * cad1,char *cad2)
@@ -30,3 +30,47 @@ char* strcat_mio(char * cad1,char *cad2)
     *cad2= '\0';
     return aux;
 }
+
+int myStrlen(const char *s)
+{
+    int cont=0;
+    while(*s)
+    {
+        cont++;
+        s++;
+    }
+    return cont;
+}
+
+/*char *myStrcpy(const char *s1, char *s2)
+{
+    char *aux= s2;
+    while(*s1)
+    {
+        *s2= *s1;
+        s1++;
+        s2++;
+    }
+    *s2= '/0';
+    return aux;
+}*/
+
+char *myStrchr(char *s, int c)
+{
+    while(*s)
+    {
+        if(*s==c)
+        {
+            return s;
+        }
+        s++;
+    }
+    return NULL;
+
+}
+
+
+
+
+
+
